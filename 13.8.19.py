8b622bec71bac7082f7ab705832b9158ac48ca23
Наталья Набиева <101559839+NatalyaNabieva@users.noreply.github.com>
2022-05-25 22:14:13 +0700

    Update 13.8.19.py

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,17 +1,17 @@
+Final_price = 0
 Tickets=int(input('Введите общее количество билетов:'))
 Age=[int(input('Введите возраст каждого посетителя:')) for i in range(Tickets)]
-listMask = []
+
 for i in Age:
     if i<18:
-        listMask.append(0) 
+        price=0
     elif 18<=i<25:
-         listMask.append(990)
+        price=990
     else:
-         listMask.append(1390) 
-Final_price=sum(listMask)
+       price=1390
+    Final_price = Final_price+price
+
 print('Вы заказали, билетов:',int(Tickets))
-print('Стоимость билетов:',(listMask),'рублей') 
-if Final_price>=2970 and int(Tickets)>=3:  
-    print('Общая стоимость билетов c учетом скидки составит:', Final_price*0.9,'рублей')
-else:
-    print('Общая стоимость билетов составит:', Final_price,'рублей')
+print('Общая стоимость билетов составит:', Final_price,'рублей')
+if Tickets>=3:
+  print('Общая стоимость билетов c учетом скидки составит:', Final_price*0.9,'рублей')
